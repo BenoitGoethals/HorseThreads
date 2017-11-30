3b5fa8f4478794b0c907030f10bfde75bd06913f
root <benoit.goethals@gmail.com>
2017-11-30 08:07:53 +0100

    init

---
+++ HorseThreads/Horse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace HorseThreads
+{
+  public   class Horse
+    {
+    
+        public string Name { get; set; }
+
+        public ManualResetEvent Start { get; set; }
+
+      
+        public AutoResetEvent Winner { get; set; }
+
+        private readonly Object _locker = new object();
+
+        public  int Row { get; set; }
+        private readonly Random _random = new Random(DateTime.Now.Millisecond);
+
+        public void Run()
+        {
+            Start.WaitOne();
+          
+            
+                for (int i = 0; i < 30; i++)
+                {
+                    var millisecondsTimeout = _random.Next(50, 500);
+                    Thread.Sleep(millisecondsTimeout);
+                    lock (_locker)
+                    {
+                        Console.ForegroundColor = (ConsoleColor)Row;
+                        Console.SetCursorPosition(i, Row);
+                        Console.Write("*");
+                    }
+            }
+            Winner.Set();
+        }
+
+
+    }
+}
---
+++ HorseThreads/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static System.Threading.AutoResetEvent;
+
+namespace HorseThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  Horse[] horses = new Horse[6];
+
+
+            ConcurrentQueue<Tuple<Thread,AutoResetEvent >> threads = new ConcurrentQueue<Tuple<Thread, AutoResetEvent>>();
+            ManualResetEvent c = new ManualResetEvent(initialState: false);
+            for (int i = 1; i < 16; i++)
+            {
+                AutoResetEvent w=   new AutoResetEvent(initialState: false);
+                Horse horse = new Horse() { Row = i, Start = c, Name=$"horse {i}", Winner=w };
+                threads.Enqueue(item: new Tuple<Thread, AutoResetEvent>(item1: new Thread(start: horse.Run ){Name = i.ToString()},item2: w));
+
+            }
+
+            foreach (var th in threads)
+            {
+                th.Item1.Start();
+            }
+
+            c.Set();
+
+
+            WaitHandle[] autoResetEvents = threads.Select(selector: ct=>ct.Item2).ToArray<AutoResetEvent>();
+            int winner = WaitHandle.WaitAny(waitHandles: autoResetEvents);
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+              Console.WriteLine(value: $"### winner is {threads.Select(selector: ct => ct.Item1).ToArray<Thread>()[winner].Name  }");
+
+            Console.WriteLine(value: "");
+            Console.ReadLine();
+        }
+
+       
+    }
+
+   
+}
